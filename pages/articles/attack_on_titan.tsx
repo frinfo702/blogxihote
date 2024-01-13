e36486c36353464710321f27938cb02539a2f1c3
後藤健一郎 <gotokenichiro@gotoukenichirounoMacBook-Air.local>
2024-01-13 17:28:43 +0900

    second commit

---
+++ pages/articles/attack_on_titan.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ArticlePage() {
+  const title = 'Attack on Titan Review';
+  const content = `
+  んおおおお!!イクッ！イクッ！ドンキホーテ・ドフラミンゴ!!41歳!!!
+  ゴミ共の前で乳首シコられながらケツアクメキメるぞォ!!!見てろよォテメェら!!フッフッフッフッフ!!!
+  `;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
